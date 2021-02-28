6b607c37d272250cae5a5219579187f641b4b2b3
MaciejBuczek <49284699+MaciejBuczek@users.noreply.github.com>
2021-02-28 14:15:58 +0100

    Added - quiz creation page client side validation

--- wwwroot/js/generationDOM.js
+++ wwwroot/js/generationDOM.js
@@ -1,72 +1,72 @@
-﻿let cardNumber = 1;
+﻿let questionCounter = 1;
 var answersNumbers = [1];
 function generateQuizQuestion() {
-    cardNumber++;
+    questionCounter++;
     answersNumbers.push(1);
-    var card = `<div class="card" id="cardQ` + cardNumber.toString() +`">
-                    <div class="card-header" id="headingQ`+ cardNumber.toString() +`">
-                        <div class="btn btn-danger float-right" onclick="removeQuestion('cardQ` + cardNumber.toString() +`')"><i class="fas fa-trash"></i></div>
+    var card = `<div class="card" id="cardQ` + questionCounter.toString() +`">
+                    <div class="card-header" id="headingQ`+ questionCounter.toString() +`">
+                        <div class="btn btn-danger float-right" onclick="removeQuestion('cardQ` + questionCounter.toString() +`')"><i class="fas fa-trash"></i></div>
                         <h1 class="mb-0">
-                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseQ`+ cardNumber.toString() + `" aria-expanded="false" aria-controls="collapseQ` + cardNumber.toString() +`">
-                                Question #`+ cardNumber.toString() +`
+                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseQ`+ questionCounter.toString() + `" aria-expanded="false" aria-controls="collapseQ` + questionCounter.toString() +`">
+                                Question #`+ questionCounter.toString() +`
                             </button>
                         </h1>
                     </div>
 
-                    <div id="collapseQ`+ cardNumber.toString() +`" class="collapse" aria-labelledby="headingQ`+ cardNumber.toString() +`" data-parent="#quizAccordion">
+                    <div id="collapseQ`+ questionCounter.toString() +`" class="collapse" aria-labelledby="headingQ`+ questionCounter.toString() +`" data-parent="#quizAccordion">
                         <div class="card-body">
                             <div>
                                 <div class="float-left mb-3">
                                     <label>Time to answer</label>
-                                    <input type="number" class="form-control mb-2" id="timeQ`+ cardNumber.toString() +`" placeholder="[s]">
+                                    <input type="number" class="form-control mb-2" id="timeQ`+ questionCounter.toString() +`" placeholder="[s]">
                                     <label>Points</label>
-                                    <input type="number" class="form-control" id="pointsQ`+ cardNumber.toString() +`">
+                                    <input type="number" class="form-control" id="pointsQ`+ questionCounter.toString() +`">
                                 </div>
-                                <div class="float-left mb-3 ml-4 w-75" id="questionContentQ`+ cardNumber.toString() +`">
+                                <div class="float-left mb-3 ml-4 w-75" id="questionContentQ`+ questionCounter.toString() +`">
                                     <label>Question</label>
                                     <div class="summernote"></div>
                                 </div>
 
-                                <div class="accordion float-left w-100 mb-4" id="accordionQ`+ cardNumber.toString() +`">
-                                    <div class="card" id="cardQ`+ cardNumber.toString() +`A1">
+                                <div class="accordion float-left w-100 mb-4" id="accordionQ`+ questionCounter.toString() +`">
+                                    <div class="card" id="cardQ`+ questionCounter.toString() +`A1">
                                         <div class="card-header heading">
-                                            <div class="btn btn-danger float-right" onclick="removeAnswer('cardQ`+ cardNumber.toString() +`A1')"><i class="fas fa-trash"></i></div>
-                                            <div class="btn btn-success float-right mr-4" onclick="setCorrectAnswer('cardQ`+ cardNumber.toString() +`A1')"><i class="fas fa-check"></i></div>
+                                            <div class="btn btn-danger float-right" onclick="removeAnswer('cardQ`+ questionCounter.toString() +`A1')"><i class="fas fa-trash"></i></div>
+                                            <div class="btn btn-success float-right mr-4" onclick="setCorrectAnswer('cardQ`+ questionCounter.toString() +`A1')"><i class="fas fa-check"></i></div>
                                             <h1 class="mb-0">
-                                                <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseQ`+ cardNumber.toString() + `A1" aria-expanded="true" aria-controls="collapseQ` + cardNumber.toString() +`A1">
+                                                <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseQ`+ questionCounter.toString() + `A1" aria-expanded="true" aria-controls="collapseQ` + questionCounter.toString() +`A1">
                                                     Answer #1
                                                 </button>
                                             </h1>
                                         </div>
-                                        <div id="collapseQ`+ cardNumber.toString() + `A1" class="collapse show" aria-labelledby="headingA1" data-parent="#accordionQ` + cardNumber.toString() +`">
-                                            <div class="card-body" id="answerContentQ`+ cardNumber.toString() +`A1">
+                                        <div id="collapseQ`+ questionCounter.toString() + `A1" class="collapse show" aria-labelledby="headingA1" data-parent="#accordionQ` + questionCounter.toString() +`">
+                                            <div class="card-body" id="answerContentQ`+ questionCounter.toString() +`A1">
                                                 <label>Answer content</label>
                                                 <div class="summernote"></div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                <button class="btn btn-primary" onclick="addAnswer('accordionQ` + cardNumber.toString() +`')"><i class="fas fa-plus"></i>&nbsp; Add Answer</button>
+                                <button class="btn btn-primary" onclick="addAnswer('accordionQ` + questionCounter.toString() +`')"><i class="fas fa-plus"></i>&nbsp; Add Answer</button>
                             </div>
                         </div>
                     </div>
                 </div>`;
     return card;
 }
-function generateQuizAnswer(cardNumber) {
-    var answerNumber = ++answersNumbers[cardNumber - 1];
-    var card = `<div class="card" id="cardQ` + cardNumber.toString() + `A` + answerNumber.toString() +`">
+function generateQuizAnswer(questionCounter) {
+    var answerNumber = ++answersNumbers[questionCounter - 1];
+    var card = `<div class="card" id="cardQ` + questionCounter.toString() + `A` + answerNumber.toString() +`">
                     <div class="card-header heading">
-                        <div class="btn btn-danger float-right" onclick="removeAnswer('cardQ`+ cardNumber.toString() + `A` + answerNumber.toString() +`')"><i class="fas fa-trash"></i></div>
-                        <div class="btn btn-success float-right mr-4" onclick="setCorrectAnswer('cardQ`+ cardNumber.toString() + `A` + answerNumber.toString() +`')"><i class="fas fa-check"></i></div>
+                        <div class="btn btn-danger float-right" onclick="removeAnswer('cardQ`+ questionCounter.toString() + `A` + answerNumber.toString() +`')"><i class="fas fa-trash"></i></div>
+                        <div class="btn btn-success float-right mr-4" onclick="setCorrectAnswer('cardQ`+ questionCounter.toString() + `A` + answerNumber.toString() +`')"><i class="fas fa-check"></i></div>
                         <h1 class="mb-0">
-                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseQ`+ cardNumber.toString() + `A` + answerNumber.toString() + `" aria-expanded="true" aria-controls="collapseQ` + cardNumber.toString() + `A` + answerNumber.toString() +`">
+                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseQ`+ questionCounter.toString() + `A` + answerNumber.toString() + `" aria-expanded="true" aria-controls="collapseQ` + questionCounter.toString() + `A` + answerNumber.toString() +`">
                                 Answer #` + answerNumber.toString() +`
                             </button>
                         </h1>
                     </div>
-                    <div id="collapseQ`+ cardNumber.toString() + `A` + answerNumber.toString() + `" class="collapse" aria-labelledby="headingA` + answerNumber.toString() +`" data-parent="#accordionQ` + cardNumber.toString() + `">
-                        <div class="card-body" id="answerContentQ`+ cardNumber.toString() + `A` + answerNumber.toString() +`">
+                    <div id="collapseQ`+ questionCounter.toString() + `A` + answerNumber.toString() + `" class="collapse" aria-labelledby="headingA` + answerNumber.toString() +`" data-parent="#accordionQ` + questionCounter.toString() + `">
+                        <div class="card-body" id="answerContentQ`+ questionCounter.toString() + `A` + answerNumber.toString() +`">
                             <label>Answer content</label>
                             <div class="summernote"></div>
                         </div>
--- wwwroot/js/validation.js
+++ wwwroot/js/validation.js
@@ -1,1 +1,52 @@
-﻿
+﻿function validateeQuiz() {
+    var errorTitle = "Quiz creation error"
+    if (document.getElementById("quizName").value == "") {
+        fireErrorAlert(errorTitle, "Please enter a quiz name");
+        return false;
+    }
+    if (questionCounter == 0) {
+        fireErrorAlert(errorTitle, "Quiz has to have at least one question");
+        return false;
+    }
+    
+    for (i = 0; i < answersNumbers.length; i++) {
+        if (answersNumbers[i] == 0) {
+            fireErrorAlert(errorTitle, "Each question has to have at least one answer");
+            return false;
+        }
+    }
+
+    var i;
+    for (i = 1; i <= questionCounter; i++) {
+        var content = document.getElementById("questionContentQ" + i).getElementsByTagName("p")[1].innerHTML;
+        if (content == "<br>" || content == "" || document.getElementById("timeQ" + i).value == "" ||
+            document.getElementById("pointsQ" + i).value == "") {
+                fireErrorAlert(errorTitle, "Some questions are missing content, time or points");
+                return false;
+        }
+        var isCorrectAnswerSet = false;
+        var j;
+        for (j = 1; j <= answersNumbers[i - 1]; j++) {
+            content = document.getElementById("answerContentQ" + i + "A" + j).getElementsByTagName("p")[1].innerHTML
+            if (content == "<br>" || content == "") {
+                fireErrorAlert(errorTitle, "Some answers are missing content");
+                return false;
+            }
+            if (document.getElementById("cardQ" + i + "A" + j).classList.contains("selected"))
+                isCorrectAnswerSet = true;
+        }
+        if (!isCorrectAnswerSet) {
+            fireErrorAlert(errorTitle, "Each question has to have at least one correct answer selected");
+            return false;
+        }
+    }
+    return true;
+}
+
+function fireErrorAlert(errorTitle, errorContent) {
+    Swal.fire(
+        errorTitle,
+        errorContent,
+        'error'
+    )
+}
