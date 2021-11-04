9a7d94523d888413e862db92a5b8f826c8ff52f4
MaciejBuczek <49284699+MaciejBuczek@users.noreply.github.com>
2021-11-04 10:41:21 +0100

    loading screen added

---
+++ wwwroot/js/utils/loader.js
@@ -0,0 +1,15 @@
+﻿const loader = document.getElementById("loader");
+const loaderText = document.getElementById("loader-text");
+
+function showLoader(text) {
+
+    loader.style.display = "block"
+
+    if(text != null)
+        loaderText.innerText = text;
+}
+
+function hideLoader() {
+    loader.style.display = "none"
+    loaderText.innerText = "";
+}
