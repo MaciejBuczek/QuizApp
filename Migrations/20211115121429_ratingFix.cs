b5a48701e00c78538a283d60ad9d7cf27479d190
MaciejBuczek <49284699+MaciejBuczek@users.noreply.github.com>
2021-11-15 13:18:17 +0100

    rating migrations

---
+++ Migrations/20211115121429_ratingFix.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace QuizApp.Migrations
+{
+    public partial class ratingFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rating_Quizzes_IdQuiz",
+                table: "Rating");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Rating",
+                table: "Rating");
+
+            migrationBuilder.RenameTable(
+                name: "Rating",
+                newName: "Ratings");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Rating_IdQuiz",
+                table: "Ratings",
+                newName: "IX_Ratings_IdQuiz");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Ratings",
+                table: "Ratings",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ratings_Quizzes_IdQuiz",
+                table: "Ratings",
+                column: "IdQuiz",
+                principalTable: "Quizzes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ratings_Quizzes_IdQuiz",
+                table: "Ratings");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Ratings",
+                table: "Ratings");
+
+            migrationBuilder.RenameTable(
+                name: "Ratings",
+                newName: "Rating");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Ratings_IdQuiz",
+                table: "Rating",
+                newName: "IX_Rating_IdQuiz");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Rating",
+                table: "Rating",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rating_Quizzes_IdQuiz",
+                table: "Rating",
+                column: "IdQuiz",
+                principalTable: "Quizzes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
