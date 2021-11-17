c2149a1fe9effdb7225aa82745d9cf39172c35f6
MaciejBuczek <49284699+MaciejBuczek@users.noreply.github.com>
2021-11-17 23:17:19 +0100

    rating js added

---
+++ wwwroot/js/utils/ratings.js
@@ -0,0 +1,63 @@
+﻿const starEmpty = "<i class='far fa-star' onclick='onClick(this)' onmouseover='onHover(this)' onmouseout='onHoverExit(this)' value=></i>";
+const startFull = "<i class='fas fa-star' onclick='onClick(this)' onmouseover='onHover(this)' onmouseout='onHoverExit(this)' value=></i>";
+let lockedStars = [false, false, false, false, false];
+
+function onHover(element) {
+    fillPreviousStars(element, false);
+}
+
+function onHoverExit(element) {
+    let stars = document.querySelectorAll("i.far, i.fas");
+
+    for (var i = 0; i < stars.length; i++) {
+        if (!lockedStars[i] && stars[i].classList.contains("fas")) {
+            stars[i].classList.remove("fas");
+            stars[i].classList.add("far");
+        }
+    } 
+}
+
+function onClick(element) {
+    fillPreviousStars(element, true);
+    alert(parseInt(element.getAttribute("value")));
+}
+
+function fillPreviousStars(element, lock) {
+    let value = parseInt(element.getAttribute("value"));
+    let stars = document.querySelectorAll("i.far, i.fas");
+    for (var i = 0; i < stars.length; i++) {     
+
+        if (parseInt(stars[i].getAttribute("value")) <= value) {
+            if (lock)
+                lockedStars[i] = true;
+
+            if (stars[i].classList.contains("far")) {
+                stars[i].classList.remove("far");
+                stars[i].classList.add("fas");
+            }
+        } else {
+            if (lock)
+                lockedStars[i] = false;
+
+            if ((lock || !lockedStars[i]) && stars[i].classList.contains("fas")) {
+                stars[i].classList.remove("fas");
+                stars[i].classList.add("far");
+            }
+        }
+    } 
+}
+
+function renderRatings(element)
+{
+    let search = "value=";
+    element.innerHtml = '';
+    for (var i = 0; i < 5; i++) {
+
+        let starString = starEmpty;
+        var index = starString.indexOf(search);
+
+        var starValueString = starString.slice(0, index + search.length) + i + starString.slice(index + search.length);
+
+        element.innerHTML += starValueString;
+    }
+}
