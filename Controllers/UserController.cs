025f26d4e6d55a06e02655a138486014b87ba49d
MaciejBuczek <49284699+MaciejBuczek@users.noreply.github.com>
2021-11-25 17:54:18 +0100

    admin user edit panel implemented

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -13,8 +13,8 @@ namespace QuizApp.Controllers
     [Authorize(Roles = Constants.Roles.AdminRole)]
     public class UserController : Controller
     {
-        private ApplicationDbContext _db;
-        private UserManager<IdentityUser> _userManager;
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
         private readonly int _resultPerPage = 10;
 
         public UserController(ApplicationDbContext db, UserManager<IdentityUser> userManager)
@@ -47,6 +47,7 @@ namespace QuizApp.Controllers
             return View(nameof(Index), userVM);
         }
 
+        [HttpPost]
         public async Task<IActionResult> Remove(string id)
         {
             if (string.IsNullOrEmpty(id))
@@ -58,6 +59,70 @@ namespace QuizApp.Controllers
 
             await _userManager.DeleteAsync(user);
             return RedirectToAction(nameof(Search));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+                return NotFound();
+
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+                return NotFound();
+
+            return View(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditUsername(string id, string username)
+        {
+            if (string.IsNullOrEmpty(id) || string.IsNullOrEmpty(username))
+                return NotFound();
+
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+                return NotFound();
+
+            user.UserName = username;
+
+            await _userManager.UpdateAsync(user);
+
+            return RedirectToAction(nameof(Edit), new { id = id });
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditEmail(string id, string email)
+        {
+            if (string.IsNullOrEmpty(id) || string.IsNullOrEmpty(email))
+                return NotFound();
+
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+                return NotFound();
+
+            user.Email = email;
+
+            await _userManager.UpdateAsync(user);
+
+            return RedirectToAction(nameof(Edit), new { id = id });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditPassword(string id, string password)
+        {
+            if (string.IsNullOrEmpty(id) || string.IsNullOrEmpty(password))
+                return NotFound();
+
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+                return NotFound();
+
+            var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+            await _userManager.ResetPasswordAsync(user, token, password);
+
+            return RedirectToAction(nameof(Edit), new { id = id });
         }
     }
 }
